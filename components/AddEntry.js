0fc54dbf4207a8b1501dac98bf4a9dd2b430f34f
Jasmine Vo <jasmine@Jasmines-MacBook-Pro.local>
2018-04-16 13:38:29 -0700

    updates to API

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,7 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
-
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -57,9 +57,13 @@ export default class AddEntry extends Component {
     const entry = this.state
 
     this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
+
+    submitEntry({ key, entry })
   }
   reset = () => {
     const key = timeToString()
+
+    removeEntry(key)
   }
   render() {
     const metaInfo = getMetricMetaInfo()
